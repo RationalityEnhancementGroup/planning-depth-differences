0c99ba03f0b376cc2b74b44213e5e8a8ffc24083
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2022-08-31 10:24:32 +0200

    add missing script: calculate_trial_by_trial_likelihoods.py

---
+++ analysis/methods/static/src/calculate_trial_by_trial_likelihoods.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from costometer.utils import AnalysisObject
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-e",
+        "--exp",
+        dest="experiment_name",
+    )
+    parser.add_argument(
+        "-s",
+        "--subdirectory",
+        dest="experiment_subdirectory",
+        metavar="experiment_subdirectory",
+    )
+    inputs = parser.parse_args()
+
+    irl_path = Path(__file__).resolve().parents[4]
+
+    analysis_obj = AnalysisObject(
+        inputs.experiment_name,
+        irl_path=irl_path,
+        experiment_subdirectory=inputs.experiment_subdirectory,
+    )
+    trial_by_trial_df = analysis_obj.get_trial_by_trial_likelihoods()
