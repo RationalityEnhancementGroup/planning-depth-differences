9879801fd9d107b97e3fd2fa59141cf1800f337d
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2022-09-20 09:20:09 +0200

    add ground truth jsons for test cases

---
+++ cluster/src/generate_test_rewards.py
@@ -0,0 +1,29 @@
+"""Used for generating ground truth files for test environments"""
+from pathlib import Path
+
+from cluster_utils import create_test_env
+from mouselab.env_utils import generate_ground_truth_file
+from mouselab.mouselab import MouselabEnv
+
+if __name__ == "__main__":
+    for experiment_setting in [
+        "small_test_case",
+        "reduced_leaf",
+        "reduced_middle",
+        "reduced_root",
+        "reduced_variance",
+    ]:
+        create_test_env(experiment_setting)
+        save_path = Path(__file__).parents[2].joinpath("data/inputs/exp_inputs/rewards")
+
+        env = MouselabEnv.new_symmetric_registered(
+            experiment_setting=experiment_setting
+        )
+
+        generate_ground_truth_file(
+            env,
+            num_ground_truths=100,
+            save_path=save_path,
+            file_name=experiment_setting,
+            seed=91,
+        )
