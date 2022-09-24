05611842dcad58f1a5d04034404062ea83d800ce
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2022-09-24 14:08:32 +0200

    docker files

---
+++ analysis/questionnaire/docker/helper_scripts/install_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+mkdir -p /home/rstudio/.R/ \
+    && echo "CXXFLAGS=-O3 -mtune=native -march=native -Wno-unused-variable -Wno-unused-function -Wno-macro-redefined" >> /home/rstudio/.R/Makevars \
+    && echo "CXXFLAGS+=-flto -Wno-unused-local-typedefs" >> /home/rstudio/.R/Makevars \
+    && echo "CXXFLAGS += -Wno-ignored-attributes -Wno-deprecated-declarations" >> /home/rstudio/.R/Makevars \
+    && echo "CXX14 = g++ -std=c++1y" >> /home/rstudio/.R/Makevars \
+    && echo "rstan::rstan_options(auto_write = TRUE)" >> /home/rstudio/.Rprofile \
+    && echo "options(mc.cores = parallel::detectCores())" >> /home/rstudio/.Rprofile
