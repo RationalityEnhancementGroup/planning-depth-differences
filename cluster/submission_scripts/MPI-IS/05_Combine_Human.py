0be8c2e03795c045ba4b4e47e10841f7f1959e2b
Valkyrie <valkyrie.felso@tuebingen.mpg.de>
2023-01-28 12:32:19 +0100

    add participant_subset_file to filenames that are loaded (not saved)

--- cluster/submission_scripts/MPI-IS/05_Combine_Human.py
+++ cluster/submission_scripts/MPI-IS/05_Combine_Human.py
@@ -82,7 +82,8 @@ if __name__ == "__main__":
     # load random file
     random_df = pd.read_csv(
         f"data/logliks/{inputs.simulated_cost_function}/"
-        f"{inputs.experiment}/RandomPolicy_optimization_results{simulation_params}.csv",
+        f"{inputs.experiment}/RandomPolicy_optimization_results{simulation_params}"
+        f"_{inputs.participant_subset_file}.csv",
         index_col=0,
     )
     random_df["applied_policy"] = "RandomPolicy"
@@ -104,7 +105,8 @@ if __name__ == "__main__":
             f"data/logliks/{inputs.simulated_cost_function}/"
             f"{inputs.experiment}/"
             f"SoftmaxPolicy_optimization_results_"
-            f"{get_param_string(cost_parameters)}{simulation_params}.csv"
+            f"{get_param_string(cost_parameters)}{simulation_params}"
+            f"_{inputs.participant_subset_file}.csv"
         )
 
         curr_df = pd.read_csv(curr_file_name, index_col=0)
