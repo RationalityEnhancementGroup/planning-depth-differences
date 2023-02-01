1cd317d0057ac02a8ba2c6b20c5a962beedceaa5
vmfelso <vmfelso@users.noreply.github.com>
2023-02-01 13:13:11 +0100

    for testing hyperopt use

--- cluster/src/infer_parameters_static_opt.py
+++ cluster/src/infer_parameters_static_opt.py
@@ -5,12 +5,11 @@ temperatures or a RandomPolicy agent.
 It uses hyperopt.
 """
 import json
+import time
 from argparse import ArgumentParser
 from pathlib import Path
-import dill as pickle
-import time
 
-from more_itertools import powerset
+import dill as pickle
 import yaml
 from cluster_utils import (
     get_args_from_yamls,
@@ -18,14 +17,13 @@ from cluster_utils import (
     get_simulated_trajectories,
 )
 from costometer.agents.vanilla import SymmetricMouselabParticipant
-from costometer.inference import HyperoptOptimizerInference, GridInference
-from mouselab.cost_functions import *  # noqa
-from mouselab.distributions import Categorical
-from mouselab.graph_utils import get_structure_properties
-from mouselab.policies import RandomPolicy, SoftmaxPolicy
-from scipy import stats  # noqa
+from costometer.inference import HyperoptOptimizerInference
+from costometer.utils import get_temp_prior
 from get_myopic_voc_values import get_state_action_values
-from hyperopt.early_stop import no_progress_loss
+from mouselab.cost_functions import *  # noqa
+from mouselab.graph_utils import get_structure_properties
+from mouselab.policies import SoftmaxPolicy
+from scipy import stats  # noqa
 
 if __name__ == "__main__":
     # get arguments
@@ -45,6 +43,14 @@ if __name__ == "__main__":
         type=str,
     )
     parser.add_argument(
+        "-t",
+        "--temperature-file",
+        default="expon",
+        dest="temperature_file",
+        help="File with temperatures to infer over",
+        type=str,
+    )
+    parser.add_argument(
         "-p",
         "--prior-file",
         dest="prior_file",
@@ -58,6 +64,12 @@ if __name__ == "__main__":
         default="Myopic_VOC",
         help="BMPS Features and Optimization",
         type=str,
+    )
+    parser.add_argument(
+        "--n",
+        "--num-evals",
+        dest="num_evals",
+        type=int,
     )
     parser.add_argument(
         "-d",
@@ -76,10 +88,7 @@ if __name__ == "__main__":
         args = get_args_from_yamls(
             vars(inputs), attributes=["cost_function", "experiment"]
         )
-        args = {**args, **get_args_from_yamls(
-            args, attributes=["experiment_setting"]
-        )
-                }
+        args = {**args, **get_args_from_yamls(args, attributes=["experiment_setting"])}
 
     path = Path(__file__).resolve().parents[2]
 
@@ -110,9 +119,7 @@ if __name__ == "__main__":
 
     if inputs.prior_file is not None:
         yaml_path = str(
-            path.joinpath(
-                f"cluster/parameters/priors/{inputs.prior_file}.yaml"
-            )
+            path.joinpath(f"cluster/parameters/priors/{inputs.prior_file}.yaml")
         )
         with open(str(yaml_path), "r") as stream:
             prior_inputs = yaml.safe_load(stream)
@@ -121,10 +128,10 @@ if __name__ == "__main__":
 
     if "structure" in args:
         with open(
-                Path(__file__)
-                        .parents[2]
-                        .joinpath(f"data/inputs/exp_inputs/structure/{args['structure']}.json"),
-                "rb",
+            Path(__file__)
+            .parents[2]
+            .joinpath(f"data/inputs/exp_inputs/structure/{args['structure']}.json"),
+            "rb",
         ) as f:
             structure_data = json.load(f)
 
@@ -132,95 +139,67 @@ if __name__ == "__main__":
     else:
         structure_dicts = None
 
+    if inputs.temperature_file is not None:
+        yaml_path = str(
+            path.joinpath(
+                f"data/inputs/yamls/temperatures/{inputs.temperature_file}.yaml"
+            )
+        )
+        with open(str(yaml_path), "r") as stream:
+            temp_priors = yaml.safe_load(stream)
+        temp_priors = get_temp_prior(
+            rv=eval(temp_priors["rv"]),
+            possible_vals=temp_priors["possible_temps"],
+            inverse=temp_priors["inverse"],
+        )
+        temp_prior_dict = dict(zip(temp_priors.vals, temp_priors.probs))
+        prior_inputs["policy_parameters"]["temp"][
+            "prior"
+        ] = lambda val: temp_prior_dict[val]
+    else:
+        temp_priors = None
+
     cost_function = eval(args["cost_function"])
     if callable(eval(args["cost_function"])):
         cost_function_name = inputs.cost_function
     else:
         cost_function_name = None
 
-    q_function_generator = lambda cost_parameters, a, g: get_state_action_values(
-        experiment_setting=args["experiment_setting"],
-        bmps_file=inputs.bmps_file,
-        cost_function=cost_function,
-        cost_parameters=cost_parameters,
-        structure=structure_dicts,
-        env_params=args["env_params"],
-        alpha=a,
-        gamma=g)
-
-    all_results = {}
-    for subset in powerset(args["constant_values"]):
-        model_name = eval(args["model_name"])[subset]
-
-        softmax_opt_object = HyperoptOptimizerInference(
-            traces=traces,
-            participant_class=SymmetricMouselabParticipant,
-            participant_kwargs={
-                "experiment_setting": args["experiment_setting"],
-                "policy_function": SoftmaxPolicy,
-                "additional_mouselab_kwargs": {"mdp_graph_properties": structure_dicts},
-            },
-            held_constant_policy_kwargs={
-                "noise": 0,
-                "q_function_generator": q_function_generator,
-            },
-            held_constant_cost_kwargs= {key : args["constant_values"][key] for key in subset},
-            policy_parameters=prior_inputs["policy_parameters"],
+    q_function_generator = (
+        lambda cost_parameters, a, g: get_state_action_values(  # noqa
+            experiment_setting=args["experiment_setting"],
+            bmps_file=inputs.bmps_file,
             cost_function=cost_function,
-            cost_parameters=prior_inputs["cost_parameters"],
-            optimization_settings={"verbose":0,"max_evals":1000,"early_stop_fn": no_progress_loss(iteration_stop_count=500, percent_increase=0.0)},
+            cost_parameters=cost_parameters,
+            structure=structure_dicts,
+            env_params=args["env_params"],
+            bmps_path=Path(__file__).parents[1].joinpath("parameters/bmps/"),
+            kappa=a,
+            gamma=g,
         )
+    )
 
-        softmax_opt_object.run()
-        all_results[model_name + " with alpha, gamma"] = softmax_opt_object.get_optimization_results()
-
-        softmax_opt_object = HyperoptOptimizerInference(
-            traces=traces,
-            participant_class=SymmetricMouselabParticipant,
-            participant_kwargs={
-                "experiment_setting": args["experiment_setting"],
-                "policy_function": SoftmaxPolicy,
-                "additional_mouselab_kwargs": {"mdp_graph_properties": structure_dicts},
-            },
-            held_constant_policy_kwargs={
-                "noise": 0,
-                "q_function_generator": q_function_generator,
-                "alpha": 1,
-                "gamma": 1,
-            },
-            held_constant_cost_kwargs= {key : args["constant_values"][key] for key in subset},
-            policy_parameters=prior_inputs["policy_parameters"],
-            cost_function=cost_function,
-            cost_parameters=prior_inputs["cost_parameters"],
-            optimization_settings={"verbose": 0, "max_evals": 1000,
-                                   "early_stop_fn": no_progress_loss(iteration_stop_count=500, percent_increase=0.0)})
-        softmax_opt_object.run()
-        all_results[model_name] = softmax_opt_object.get_optimization_results()
-
-    random_opt_object = GridInference(
+    softmax_opt_object = HyperoptOptimizerInference(
         traces=traces,
         participant_class=SymmetricMouselabParticipant,
         participant_kwargs={
             "experiment_setting": args["experiment_setting"],
-            "policy_function": RandomPolicy,
-            "additional_mouselab_kwargs": {
-                "mdp_graph_properties": structure_dicts,
-                **args["env_params"],
-            },
+            "policy_function": SoftmaxPolicy,
+            "additional_mouselab_kwargs": {"mdp_graph_properties": structure_dicts},
         },
+        held_constant_policy_kwargs={
+            "noise": 0,
+            "q_function_generator": q_function_generator,
+        },
+        held_constant_cost_kwargs={},
+        policy_parameters=prior_inputs["policy_parameters"],
         cost_function=cost_function,
-        cost_parameters={
-            cost_parameter_arg: Categorical([cost_parameter_val], [1])
-            for cost_parameter_arg, cost_parameter_val in args[
-                "constant_values"
-            ].items()
-        },
-        cost_function_name=cost_function_name,
+        cost_parameters=prior_inputs["cost_parameters"],
+        optimization_settings={"verbose": 0, "max_evals": inputs.num_evals},
     )
 
-    random_opt_object.run()
-    optimization_results = random_opt_object.get_output_df()
-    all_results["Null"] = optimization_results
+    softmax_opt_object.run()
+    all_results = softmax_opt_object.get_optimization_results()
 
     # make experiment folder if it doesn't already exist
     path.joinpath(
@@ -230,7 +209,8 @@ if __name__ == "__main__":
     filename = path.joinpath(
         f"cluster/data/logliks_opt/{cost_function_name}/{experiment_folder}/"
         f"SoftmaxPolicy_optimization_results"
-        f"{simulation_params}_{inputs.pid}_{time.strftime('%Y%m%d-%H%M%S')}.csv"
+        f"{simulation_params}_{inputs.pid}_"
+        f"{inputs.num_evals}_{time.strftime('%Y%m%d-%H%M%S')}.csv"
     )
     with open(filename, "wb") as f:
-        pickle.dump(all_results,f)
+        pickle.dump(all_results, f)
