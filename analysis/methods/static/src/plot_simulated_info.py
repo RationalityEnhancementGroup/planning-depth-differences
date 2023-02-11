aa3af17f129174c7d8ab8480e7155c5ff4c9586b
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2023-02-11 19:29:13 +0100

    separate out simulation scripts

--- analysis/methods/static/src/plot_simulated_info.py
+++ analysis/methods/static/src/plot_simulated_info.py
@@ -2,14 +2,14 @@
 Plots information about the number of nodes clicked by the optimal policy
 using the Q values under different cost parameters
 """
+import itertools
 from argparse import ArgumentParser
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import seaborn as sns
 import yaml
-from costometer.utils import set_font_sizes
+from costometer.utils import AnalysisObject, set_font_sizes
 
 set_font_sizes()
 
@@ -25,36 +25,37 @@ def plot_heat_map_for_simulated(sum_df, field, rew1, rew2, name1, name2):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        "-c", "--cost", dest="cost_function", default="dist_depth_eff_forw"
+        "-e",
+        "--exp",
+        dest="experiment_name",
+        default="SoftmaxRecovery",
     )
     parser.add_argument(
-        "-e",
-        "--experiment-setting",
-        dest="experiment_setting",
-        default="high_increasing",
+        "-s",
+        "--subdirectory",
+        default="methods/static",
+        dest="experiment_subdirectory",
+        metavar="experiment_subdirectory",
     )
     inputs = parser.parse_args()
 
     irl_path = Path(__file__).resolve().parents[4]
     subdirectory = irl_path.joinpath(f"analysis/{inputs.experiment_subdirectory}")
 
-    optimal_df = pd.read_csv(
-        irl_path.joinpath(f"cluster/data/OptimalQ/OptimalQ_{inputs.cost_function}.csv")
+    analysis_obj = AnalysisObject(
+        inputs.experiment_name,
+        irl_path=irl_path,
+        experiment_subdirectory=inputs.experiment_subdirectory,
     )
 
-    yaml_file = irl_path.joinpath(
-        f"data/inputs/yamls/cost_functions/{inputs.cost_function}.yaml"
-    )
-    with open(str(yaml_file), "r") as stream:
-        cost_details = yaml.safe_load(stream)
+    mouselab_data = analysis_obj.dfs["mouselab-mdp"]
 
     sim_cost_parameters = [
-        f"sim_{param}" for param in cost_details["constant_values"].keys()
+        f"sim_{param}" for param in analysis_obj.cost_details["constant_values"].keys()
     ]
-    cost_parameter_names = cost_details["cost_parameter_names"]
 
     yaml_file = irl_path.joinpath(
-        f"data/inputs/yamls/experiment_settings/{inputs.experiment_setting}.yaml"
+        f"data/inputs/yamls/experiment_settings/{analysis_obj.experiment_setting}.yaml"
     )
     with open(str(yaml_file), "r") as stream:
         experiment_setting_details = yaml.safe_load(stream)
@@ -62,12 +63,12 @@ if __name__ == "__main__":
     for classification, nodes in experiment_setting_details[
         "node_classification"
     ].items():
-        optimal_df[classification] = optimal_df["actions"].apply(
+        mouselab_data[classification] = mouselab_data["actions"].apply(
             lambda action: action in nodes
         )
 
     sum_clicks = (
-        optimal_df.groupby(["pid", "i_episode", *sim_cost_parameters])
+        mouselab_data.groupby(["pid", "i_episode", *sim_cost_parameters])
         .sum()
         .reset_index()
         .groupby(sim_cost_parameters)
@@ -75,13 +76,16 @@ if __name__ == "__main__":
         .reset_index()
     )
 
+    sim_latex_mapping = {
+        f"sim_{k}": v for k, v in analysis_obj.cost_details["latex_mapping"].items()
+    }
     for curr_field in list(experiment_setting_details["node_classification"].keys()):
         if len(sim_cost_parameters) == 2:
             plot_heat_map_for_simulated(
                 sum_clicks,
                 curr_field,
                 *sim_cost_parameters,
-                *cost_parameter_names,
+                *[sim_latex_mapping[parameter] for parameter in sim_cost_parameters],
             )
             plt.title(f"{curr_field.title()}")
             plt.savefig(
@@ -89,26 +93,16 @@ if __name__ == "__main__":
                 bbox_inches="tight",
             )
         else:
-            held_cost_parameter = sim_cost_parameters[2]
-            held_cost_parameter_name = cost_parameter_names[2]
-            for held_cost_parameter_value in sum_clicks[held_cost_parameter].unique():
-                curr_sum_df = sum_clicks[
-                    sum_clicks[held_cost_parameter] == held_cost_parameter_value
-                ].reset_index()
+            for subset in itertools.combinations(sim_cost_parameters, 2):
                 plot_heat_map_for_simulated(
-                    curr_sum_df,
+                    sum_clicks.groupby(list(subset), as_index=False).mean(),
                     curr_field,
-                    *sim_cost_parameters[:2],
-                    *cost_parameter_names[:2],
-                )
-                plt.title(
-                    f"{curr_field.title()}, {held_cost_parameter_name}"
-                    f"={held_cost_parameter_value}"
+                    *subset,
+                    *[sim_latex_mapping[parameter] for parameter in subset],
                 )
                 plt.savefig(
                     subdirectory.joinpath(
-                        f"figs/optimal_{curr_field}_{held_cost_parameter_name}"
-                        f"_{held_cost_parameter_value}.png"
+                        f"figs/optimal_{curr_field}_{'_'.join(sorted(subset))}.png"
                     ),
                     bbox_inches="tight",
                 )
