9467893bc5a999e7972a8334b83d6a629130bb8a
vmfelso <vmfelso@users.noreply.github.com>
2023-02-19 13:19:02 +0100

    factor scores, separating two quest pilots

--- analysis/questionnaire/src/calculate_factor_scores.py
+++ analysis/questionnaire/src/calculate_factor_scores.py
@@ -1,8 +1,7 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
-import pandas as pd
-import yaml
+from costometer.utils import AnalysisObject
 from quest_utils.factor_utils import get_psychiatric_scores, load_weights
 
 if __name__ == "__main__":
@@ -10,37 +9,44 @@ if __name__ == "__main__":
     parser.add_argument(
         "-e",
         "--exp",
+        default="QuestPilot",
         dest="experiment_name",
+    )
+    parser.add_argument(
+        "-s",
+        "--subdirectory",
+        default="questionnaire",
+        dest="experiment_subdirectory",
+        metavar="experiment_subdirectory",
     )
     inputs = parser.parse_args()
 
     data_path = Path(__file__).resolve().parents[1]
     irl_path = Path(__file__).resolve().parents[3]
 
-    with open(
-        data_path.joinpath(f"inputs/yamls/{inputs.experiment_name}.yaml"), "r"
-    ) as stream:
-        experiment_arguments = yaml.safe_load(stream)
+    analysis_obj = AnalysisObject(
+        inputs.experiment_name,
+        irl_path=irl_path,
+        experiment_subdirectory=inputs.experiment_subdirectory,
+    )
 
     weights = load_weights(
-        data_path.joinpath(f"inputs/loadings/{experiment_arguments['loadings']}.csv"),
-        wise_weights=True,
+        data_path.joinpath(f"inputs/loadings/{analysis_obj.loadings}.csv"),
+        wise_weights=True if "Wise" in analysis_obj.loadings else False,
     )
 
-    individual_items = pd.concat(
-        [
-            pd.read_csv(
-                irl_path.joinpath(f"data/processed/{session}/individual_items.csv")
-            )
-            for session in experiment_arguments["sessions"]
-        ]
-    )
-    individual_items = individual_items[individual_items.columns.difference(["gender"])]
-    individual_items = individual_items.set_index("pid")
+    individual_items = analysis_obj.dfs["individual_items"]
+    individual_items = individual_items[
+        individual_items.columns.difference(["gender", "session"])
+    ]
 
     scores = get_psychiatric_scores(individual_items, weights, scale_cols=True)
 
     data_path.joinpath(f"data/{inputs.experiment_name}").mkdir(
         parents=True, exist_ok=True
     )
-    scores.to_csv(data_path.joinpath(f"data/{inputs.experiment_name}/scores.csv"))
+    scores.to_csv(
+        data_path.joinpath(
+            f"data/{inputs.experiment_name}/{analysis_obj.loadings}_scores.csv"
+        )
+    )
